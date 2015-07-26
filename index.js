805b14350a1a81cf0ac4615673714a2394601d41
wlsdml1114 <wlsdml1114@naver.com>
2015-07-26 17:22:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var request = require('request');
 var bodyParser = require('body-parser');
 var app = express();
-var c1= {
+var $ = require('jquery');
+var la101= {
     "id" : 101,
     "temp" : 0,
     "lum" : 0,
@@ -12,7 +13,7 @@ var c1= {
     "aircon" : 0,
     "proj" : 0
 };
-var c2= {
+var la102= {
     "id" : 102,
     "temp" : 0,
     "lum" : 0,
@@ -21,43 +22,93 @@ var c2= {
     "aircon" : 0,
     "proj" : 0
 };
-
+var req1= {
+    "id" : 101,
+    "temp" : 0,
+    "lum" : 0,
+    "hum" : 0,
+    "inuse" : 0,
+    "aircon" : 0,
+    "proj" : 0
+};
+var req2= {
+    "id" : 102,
+    "temp" : 0,
+    "lum" : 0,
+    "hum" : 0,
+    "inuse" : 0,
+    "aircon" : 0,
+    "proj" : 0
+};
+var req1_bool=0;
+var req2_bool=0;
 
 app.use(bodyParser.json());
 
 
 app.get('/1',function(req,res,next){
      res.setHeader('Content-Type', 'application/json');
-     res.send(c1);
+     res.send(la101);
 
 });
 
 app.get('/2',function(req,res,next){
      res.setHeader('Content-Type', 'application/json');
-     res.send(c2);
+     res.send(la102);
 
 });
 app.post('/',function(req,res,next){
     if(req.body.id==101){
-        c1.temp = req.body.temp;
-        c1.lum = req.body.lum;
-        c1.hum = req.body.hum;
-        c1.inuse = req.body.inuse;
-        c1.aircon = req.body.aircon;
-        c1.proj = req.body.proj;
-        res.send(c1);
+        if(req.body.control==0){
+            la101.temp = req.body.temp;
+            la101.lum = req.body.lum;
+            la101.hum = req.body.hum;
+            la101.inuse = req.body.inuse;
+            la101.aircon = req.body.aircon;
+            la101.proj = req.body.proj;
+            if(req1_bool==1){
+                res.send(req1);
+                req1_bool=0;
+            }
+            else{
+                res.send("nothing request");
+            }
+        }
+        else{
+            req1.temp = req.body.temp;
+            req1.lum = req.body.lum;
+            req1.hum = req.body.hum;
+            req1.inuse = req.body.inuse;
+            req1.aircon = req.body.aircon;
+            req1.proj = req.body.proj;  
+            req1_bool=1;
+        }
     }else{
-
-        c2.temp = req.body.temp;
-        c2.lum = req.body.lum;
-        c2.hum = req.body.hum;
-        c2.inuse = req.body.inuse;
-        c2.aircon = req.body.aircon;
-        c2.proj = req.body.proj;
-        res.send(c2);
+        if(req.body.control==0){
+            la102.temp = req.body.temp;
+            la102.lum = req.body.lum;
+            la102.hum = req.body.hum;
+            la102.inuse = req.body.inuse;
+            la102.aircon = req.body.aircon;
+            la102.proj = req.body.proj;
+            if(req2_bool==1){
+                res.send(req2);
+                req2_bool=0;
+            }
+            else{
+                res.send("nothing request");
+            }
+        }
+        else{
+            req2.temp = req.body.temp;
+            req2.lum = req.body.lum;
+            req2.hum = req.body.hum;
+            req2.inuse = req.body.inuse;
+            req2.aircon = req.body.aircon;
+            req2.proj = req.body.proj;  
+            req2_bool=1;
+        }
     }
 });
-
- 
 app.listen(3000);
 console.log('Listening on port 3000...');/////
